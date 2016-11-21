6c546bdce82aaddc17594bfd84645ce1e976cb8f
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2016-11-21 19:27:51 -0200

    Correcao de icone e de servidor

--- www/js/services.js
+++ www/js/services.js
@@ -2,8 +2,7 @@ angular.module('starter.services', [])
 
 .factory('ServerName', function(){
 
-  //var serverName = "http://localhost:8000";
-  var serverName = "http://valinhos.ime.usp.br:51080";
+  var serverName = "http://www.arquigrafia.org.br";
 
   return {
     get: function () {
